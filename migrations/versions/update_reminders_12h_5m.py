cb4f73131b00a34168bc6bbf614da11b867d51bf
Mauricio Diaz Cabrera <mdiazcabr@gmail.com>
2026-04-19 22:38:17 -0600

    fix: unificación de cabezas de Alembic re-encadenando recordatorios de citas

--- migrations/versions/update_reminders_12h_5m.py
+++ migrations/versions/update_reminders_12h_5m.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'update_reminders_12h_5m'
-down_revision = 'add_appointments_and_core'
+down_revision = 'sessions_add_user_001'
 branch_labels = None
 depends_on = None
 
